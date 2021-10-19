780d9d582da9ae46e9e5deb24cf00845f4df7516
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2021-10-19 19:43:24 +0000

    mm: __set_page_dirty uses spin_lock_irqsave instead of spin_lock_irq

--- src/bin/compute_mnist.rs
+++ src/bin/compute_mnist.rs
@@ -1,3 +1,3 @@
 use deep_learning_playground::neural_network::{self, activate_functions};
 use deep_learning_playground::setup::dlfs::chap3;
-use deep_learning_play
+use deep_learning_playground::setup::mnist::{batched, load_data, tes
