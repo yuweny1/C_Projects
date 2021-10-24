9e7407d70dc12205587d5bbb8470b20d5039159a
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2021-10-24 17:43:29 +0000

    Ignore build dir

--- src/bin/compute_mnist.rs
+++ src/bin/compute_mnist.rs
@@ -1,4 +1,4 @@
 use deep_learning_playground::neural_network::{self, activate_functions};
 use deep_learning_playground::setup::dlfs::chap3;
 use deep_learning_playground::setup::mnist::{batched, load_data, test_dataset, Batched};
-use deep_learning_
+use deep_learning_playground::utils::natural_transform
