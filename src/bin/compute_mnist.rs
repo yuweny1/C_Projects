68ae6d3023fc7c8cceea4d38e9d458a8329526a9
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2021-11-06 21:56:33 +0000

    Fix constant

--- src/bin/compute_mnist.rs
+++ src/bin/compute_mnist.rs
@@ -1,4 +1,7 @@
 use deep_learning_playground::neural_network::{self, activate_functions};
 use deep_learning_playground::setup::dlfs::chap3;
 use deep_learning_playground::setup::mnist::{batched, load_data, test_dataset, Batched};
-use deep_learning_playground::utils::natural_transform
+use deep_learning_playground::utils::natural_transform::to_io;
+use std::env;
+use std::io;
+use
