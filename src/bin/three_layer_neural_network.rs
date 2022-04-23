79310af45605d29d357723370904229a5de5946d
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-04-23 16:28:29 +0000

    Add screenshots to readme

--- src/bin/three_layer_neural_network.rs
+++ src/bin/three_layer_neural_network.rs
@@ -2,4 +2,7 @@ extern crate deep_learning_playground;
 extern crate ndarray;
 
 use deep_learning_playground::neural_network;
-use deep_learning_playground::neural_network::activate_func
+use deep_learning_playground::neural_network::activate_functions;
+
+fn main() {
+    if let Ok(mut n) = neur
