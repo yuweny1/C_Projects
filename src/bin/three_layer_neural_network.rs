7a4605a680f8aa018a16aa0624b558131c7baae6
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-04-23 19:36:58 +0000

    correct spelling mistakes

--- src/bin/three_layer_neural_network.rs
+++ src/bin/three_layer_neural_network.rs
@@ -5,4 +5,6 @@ use deep_learning_playground::neural_network;
 use deep_learning_playground::neural_network::activate_functions;
 
 fn main() {
-    if let Ok(mut n) = neural_network::NeuralNetwork::<f64>::new(ndarray::array![[1.0, 0.5]]
+    if let Ok(mut n) = neural_network::NeuralNetwork::<f64>::new(ndarray::array![[1.0, 0.5]]) {
+        let weights = [
+            &ndarra
