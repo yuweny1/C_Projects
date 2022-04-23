ea9b7e3d5762ba5aadef39b40bb1bd39808bb05e
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-04-23 14:56:40 +0000

    Update tests submodule

--- src/bin/three_layer_neural_network.rs
+++ src/bin/three_layer_neural_network.rs
@@ -2,4 +2,4 @@ extern crate deep_learning_playground;
 extern crate ndarray;
 
 use deep_learning_playground::neural_network;
-use deep_learning_
+use deep_learning_playground::neural_network::activate_func
