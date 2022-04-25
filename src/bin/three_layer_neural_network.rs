9ea31b7d685f7014ccb9f2b456bedd368a228e22
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-04-25 07:02:37 +0000

    Update files.

--- src/bin/three_layer_neural_network.rs
+++ src/bin/three_layer_neural_network.rs
@@ -7,4 +7,4 @@ use deep_learning_playground::neural_network::activate_functions;
 fn main() {
     if let Ok(mut n) = neural_network::NeuralNetwork::<f64>::new(ndarray::array![[1.0, 0.5]]) {
         let weights = [
-            &ndarra
+            &ndarray::array![[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]],
