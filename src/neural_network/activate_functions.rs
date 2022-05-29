b5f251922b6c43d3923cd087378b446b392c5d27
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-05-29 13:56:51 +0000

    image add

--- src/neural_network/activate_functions.rs
+++ src/neural_network/activate_functions.rs
@@ -2,4 +2,7 @@ use ndarray::Array2;
 use ndarray_stats::QuantileExt;
 use num::Float;
 
-/// `identity` generates the id
+/// `identity` generates the identity function
+/// \\(
+/// \text{id}(x)=x
+/// \\) f
