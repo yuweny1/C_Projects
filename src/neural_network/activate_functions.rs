5f1e36180ec3e9a76557883ea682f5eb9a317fc6
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-06-04 21:02:07 +0000

    Update init.sls

--- src/neural_network/activate_functions.rs
+++ src/neural_network/activate_functions.rs
@@ -12,4 +12,5 @@ pub fn identity<T>() -> Box<dyn Fn(Array2<T>) -> Array2<T>> {
 
 /// `sigmoid` generates the sigmoid function
 /// \\[
-/// S(x)=\dfrac{1
+/// S(x)=\dfrac{1}{1+\exp(-x)}
+/// \\] for `Array2` w
