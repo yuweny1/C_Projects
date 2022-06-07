94ad5b15f03b1c1cb5e09a73993d92c97b919233
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-06-07 11:14:20 +0000

    ext4: correctly migrate a file with a hole at the beginning

--- src/neural_network/activate_functions.rs
+++ src/neural_network/activate_functions.rs
@@ -15,4 +15,4 @@ pub fn identity<T>() -> Box<dyn Fn(Array2<T>) -> Array2<T>> {
 /// S(x)=\dfrac{1}{1+\exp(-x)}
 /// \\] for `Array2` where \\(x\in\mathbb{R}\\).
 pub fn sigmoid<T: Float>() -> Box<dyn Fn(Array2<T>) -> Array2<T>> {
-    Bo
+    Box::new(|x| -> Array2<T> { x.map(|val| T::one(
