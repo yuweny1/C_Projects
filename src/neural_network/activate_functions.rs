099fedb7163b990d629b49269c684c72675fb0ee
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-06-07 09:03:09 +0000

    new release

--- src/neural_network/activate_functions.rs
+++ src/neural_network/activate_functions.rs
@@ -14,4 +14,5 @@ pub fn identity<T>() -> Box<dyn Fn(Array2<T>) -> Array2<T>> {
 /// \\[
 /// S(x)=\dfrac{1}{1+\exp(-x)}
 /// \\] for `Array2` where \\(x\in\mathbb{R}\\).
-pub fn sigmoid<T: Float>
+pub fn sigmoid<T: Float>() -> Box<dyn Fn(Array2<T>) -> Array2<T>> {
+    Bo
