93e93dee325fceada64702f80e8c8f0fee0199ba
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-06-13 07:36:15 +0000

    Dropbox changes

--- src/neural_network/activate_functions.rs
+++ src/neural_network/activate_functions.rs
@@ -15,4 +15,7 @@ pub fn identity<T>() -> Box<dyn Fn(Array2<T>) -> Array2<T>> {
 /// S(x)=\dfrac{1}{1+\exp(-x)}
 /// \\] for `Array2` where \\(x\in\mathbb{R}\\).
 pub fn sigmoid<T: Float>() -> Box<dyn Fn(Array2<T>) -> Array2<T>> {
-    Box::new(|x| -> Array2<T> { x.map(|val| T::one(
+    Box::new(|x| -> Array2<T> { x.map(|val| T::one() / (T::one() + (-*val).exp())) })
+}
+
+/// `
