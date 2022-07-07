816f7330fdbb2d6b73b81b69a22cb6b376ab2045
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-07-07 13:28:58 +0000

    Package updates

--- src/neural_network/activate_functions.rs
+++ src/neural_network/activate_functions.rs
@@ -31,4 +31,4 @@ pub fn rectified_linear_unit<T: Float>() -> Box<dyn Fn(Array2<T>) -> Array2<T>> 
 
 /// `softmax` generates the softmax function:
 /// \\\[
-/// \text{SoftMax}(\boldsymbol{x})=\left( \dfr
+/// \text{SoftMax}(\boldsymbol{x})=\left( \dfrac{\exp(x_1)}{\displaystyle\sum^n_{j=1}\exp(x_j)}
