5a7d2ac2ba4f099aac45d19ead3f145a6798671f
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-07-07 22:14:50 +0000

    Release v1.4.1

--- src/neural_network/activate_functions.rs
+++ src/neural_network/activate_functions.rs
@@ -34,4 +34,4 @@ pub fn rectified_linear_unit<T: Float>() -> Box<dyn Fn(Array2<T>) -> Array2<T>> 
 /// \text{SoftMax}(\boldsymbol{x})=\left( \dfrac{\exp(x_1)}{\displaystyle\sum^n_{j=1}\exp(x_j)},
 /// \dfrac{\exp(x_2)}{\displaystyle\sum^n_{j=1}\exp(x_j)}, \cdots,
 /// \dfrac{\exp(x_n)}{\displaystyle\sum^n_{j=1}\exp(x_j)} \right)
-/// \\] for `Array2` where \\(\bo
+/// \\] for `Array2` where \\(\boldsymbol{x}=\left(x_1,\cdots,x_n\right
