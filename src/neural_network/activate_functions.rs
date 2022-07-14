f32d1148365821e1429ad19a3aab0f21481013bc
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-07-14 18:07:31 +0000

    CCE

--- src/neural_network/activate_functions.rs
+++ src/neural_network/activate_functions.rs
@@ -40,4 +40,4 @@ pub fn rectified_linear_unit<T: Float>() -> Box<dyn Fn(Array2<T>) -> Array2<T>> 
 /// \begin{array}{lll}
 /// \dfrac{\exp(x_i)}{\displaystyle\sum^n_{j=1}\exp(x_j)}&=&\dfrac{C\exp(x_i)}{C\displaystyle\sum^n_{j=1}\exp(x_j)}\\\\
 /// &=&\dfrac{\exp(x_i+\log C)}{\displaystyle\sum^n_{j=1}\exp(x_j+\log C)}\\\\
-/// &=&\d
+/// &=&\dfrac{\exp(x_i+C')}{\displaystyle\sum^n_{j=1}\exp
