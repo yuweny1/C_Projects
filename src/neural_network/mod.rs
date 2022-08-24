45e60cb0cf7e62232de943401d21b0ed5b1ee950
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-08-24 14:36:45 +0000

    email change

--- src/neural_network/mod.rs
+++ src/neural_network/mod.rs
@@ -24,4 +24,5 @@ impl<T: Float + 'static> NeuralNetwork<T> {
     /// # Arguments
     ///
     /// * `init_neurons` - The initial matrix \\(\mathbb{R}^{n\times m}\\).
-    pub fn new(init_neurons: Array2<T>) -> Result
+    pub fn new(init_neurons: Array2<T>) -> Result<Self, Error> {
+        if init_neuron
