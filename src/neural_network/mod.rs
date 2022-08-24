0eee1c859767b0b4504fc4a440e9c188fc2a981b
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-08-24 12:41:03 +0000

    Added Credits

--- src/neural_network/mod.rs
+++ src/neural_network/mod.rs
@@ -24,4 +24,4 @@ impl<T: Float + 'static> NeuralNetwork<T> {
     /// # Arguments
     ///
     /// * `init_neurons` - The initial matrix \\(\mathbb{R}^{n\times m}\\).
-  
+    pub fn new(init_neurons: Array2<T>) -> Result
