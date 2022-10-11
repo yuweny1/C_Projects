189e89831f7f390a9c311ebed18ff43f0601159b
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-10-11 13:04:32 +0000

    Doc improvements.

--- src/neural_network/mod.rs
+++ src/neural_network/mod.rs
@@ -70,4 +70,4 @@ impl<T: Float + 'static> NeuralNetwork<T> {
     /// # Arguments
     ///
     /// * `weight` - Weight matrix \\(W^{n_W\times m_W\\) for computing next neuron.
-    /// * `bias` - Bias matrix \\(B
+    /// * `bias` - Bias matrix \\(B^{n_B\times m_B}\\) for computing next neur
