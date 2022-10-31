51ad5ed0ef05ab1a5f9c7e9f57972a7ae8e31774
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-10-31 22:19:18 +0000

    Build and Deploy

--- src/perceptron/multi.rs
+++ src/perceptron/multi.rs
@@ -3,4 +3,4 @@ extern crate num;
 use super::single::{and_perceptron, nand_perceptron, or_perceptron};
 
 /// `xor_perceptron` generates the XOR function.
-/// Let \\(p\\) and \\(q\\) are logical var
+/// Let \\(p\\) and \\(q\\) are logical variables, `xor_perceptron` generates the func
