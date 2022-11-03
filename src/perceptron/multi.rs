b02559ca6fbcd6790d7a74caf6e50ef6b788a011
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-11-03 18:51:35 +0000

    Warning

--- src/perceptron/multi.rs
+++ src/perceptron/multi.rs
@@ -3,4 +3,5 @@ extern crate num;
 use super::single::{and_perceptron, nand_perceptron, or_perceptron};
 
 /// `xor_perceptron` generates the XOR function.
-/// Let \\(p\\) and \\(q\\) are logical variables, `xor_perceptron` generates the func
+/// Let \\(p\\) and \\(q\\) are logical variables, `xor_perceptron` generates the function which
+/// satisfies th
