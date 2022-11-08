287f1c257b1136ad5f20ba60a5415ef1a7636b00
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-11-08 15:11:32 +0000

    Deploy wangzhe3224/wangzhe3224.github.io to github.com/wangzhe3224/wangzhe3224.github.io.git:master

--- src/perceptron/multi.rs
+++ src/perceptron/multi.rs
@@ -4,4 +4,6 @@ use super::single::{and_perceptron, nand_perceptron, or_perceptron};
 
 /// `xor_perceptron` generates the XOR function.
 /// Let \\(p\\) and \\(q\\) are logical variables, `xor_perceptron` generates the function which
-/// satisfies th
+/// satisfies the following truth table.
+///
+/// | \
