1f3aae881f1b3d8e602a27867cee3957a4724ffa
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-12-12 21:19:37 +0000

    ALSA: usb-audio: Fix memory leak in FTU quirk

--- src/setup/dlfs/chap3.rs
+++ src/setup/dlfs/chap3.rs
@@ -3,4 +3,6 @@ use super::super::super::utils::natural_transform::to_io;
 use ndarray::Array2;
 use numpy::{PyArray1, PyArray2};
 use pyo3::prelude::{ObjectProtocol, PyResult, Python};
-use p
+use pyo3::types::IntoPyDict;
+use std::fmt;
+use 
