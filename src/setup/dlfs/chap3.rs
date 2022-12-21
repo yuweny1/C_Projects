640762eade44d2566894b7bf4f8bdda10fee2c38
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-12-21 12:31:34 +0000

    0.14.0

--- src/setup/dlfs/chap3.rs
+++ src/setup/dlfs/chap3.rs
@@ -8,4 +8,4 @@ use std::fmt;
 use std::io;
 use std::vec::Vec;
 
-const URL_BASE: &'static str = "https://github.com/or
+const URL_BASE: &'static str = "https://github.com/oreilly-japan/deep-learning-from-scratc
