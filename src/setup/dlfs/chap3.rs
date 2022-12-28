314b386831ee76b25ff361a83ebfb931107587eb
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2022-12-28 14:19:27 +0000

    prog

--- src/setup/dlfs/chap3.rs
+++ src/setup/dlfs/chap3.rs
@@ -11,4 +11,5 @@ use std::vec::Vec;
 const URL_BASE: &'static str = "https://github.com/oreilly-japan/deep-learning-from-scratch/blob/0dda3d1715e2431b76eb4089b60881948853ba2a/ch03/";
 const WEIGHT_SAVE_DIR: &'static str = ".weight_data";
 const FILE_NAME: &'static str = "sample_weight.pkl";
-const WEIGHT_NAMES: [&'static str; 3] = ["W1", "
+const WEIGHT_NAMES: [&'static str; 3] = ["W1", "W2", "W3"];
+const BIAS_NAMES: [&'s
