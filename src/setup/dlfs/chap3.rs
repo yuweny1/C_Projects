612380cd22fd0338af31c480a9346ef6bbdddc70
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2023-02-01 18:55:31 +0000

    Update to 0.3.0

--- src/setup/dlfs/chap3.rs
+++ src/setup/dlfs/chap3.rs
@@ -35,4 +35,4 @@ fn deserialize<'py>(
     fname: &str,
 ) -> PyResult<io::Result<Chap3Param>> {
     if client.dir_client.exists() && client.dir_client.file_exists(fname) {
-        if let Some(path)
+        if let Some(path) = client.dir_client.file_path(fn
