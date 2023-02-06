282cfe3d00957241e2619fb7bb6fb5e071c6584e
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2023-02-06 18:08:28 +0000

    Sound Control 3.x: adaptation for HTC One (M8)

--- src/setup/dlfs/chap3.rs
+++ src/setup/dlfs/chap3.rs
@@ -47,4 +47,5 @@ fn deserialize<'py>(
             let mut bias: Vec<Array2<f32>> = vec![];
 
             for w in WEIGHT_NAMES.iter() {
-                let code = "pickle.load(io.open('".to_owned() + path + "','rb'))['" + w 
+                let code = "pickle.load(io.open('".to_owned() + path + "','rb'))['" + w + "']";
+                let pyarray: &PyA
