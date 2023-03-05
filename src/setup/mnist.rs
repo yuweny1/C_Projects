8e2a64f2660a988418a83bc0e783e6676ecd7ec6
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2023-03-05 10:09:03 +0000

    Pontoon: Update Indonesian (id) localization of Common Voice

--- src/setup/mnist.rs
+++ src/setup/mnist.rs
@@ -8,3 +8,42 @@ extern crate tokio;
 use super::super::utils::fetch_client::{FConf, FetchClient, RemoteFile};
 use super::super::utils::natural_transform::to_io;
 use byteorder::{BigEndian, ReadBytesExt};
+use failure::Error;
+use libflate::gzip::Decoder;
+use ndarray::{stack, Array2, Axis};
+use std::fmt;
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter, Cursor, Read};
+use std::vec;
+
+#[repr(usize)]
+#[derive(Clone)]
+enum KeyFile {
+    TrainImg,
+    TrainLabel,
+    TestImg,
+    TestLabel,
+}
+
+#[derive(Clone)]
+pub struct DatasetKey {
+    img: KeyFile,
+    label: KeyFile,
+}
+
+pub fn train_dataset() -> DatasetKey {
+    DatasetKey {
+        img: KeyFile::TrainImg,
+        label: KeyFile::TrainLabel,
+    }
+}
+
+pub fn test_dataset() -> DatasetKey {
+    DatasetKey {
+        img: KeyFile::TestImg,
+        label: KeyFile::TestLabel,
+    }
+}
+
+struct MnistData {
+    sizes: vec::Vec<i32>,
